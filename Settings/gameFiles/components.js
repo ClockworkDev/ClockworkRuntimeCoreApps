0e632b808e33ac7fb75ac4376d8c782bd968b785
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-02-07 16:31:28 +0100

    Install and reset functions in the Settings app

--- Settings/gameFiles/components.js
+++ Settings/gameFiles/components.js
@@ -1,10 +1,47 @@
-HYPERGAP.presets.push([
+CLOCKWORKRT.components.push([
     {
         name: "menu",
         events: [
             {
                 name: "#setup", code: function (event) {
-                    this.engine.getEngineVar("#DOM").innerHTML = "Hello World form the menu";
+                    var that = this;
+                    createButton("Back", "back");
+                    createButton("Sideload app", "install");
+                    createButton("Reset Clockwork and erase all data", "reset");
+                    
+                    function createButton(name, event) {
+                        var button = document.createElement('div');
+                        button.innerHTML = name;
+                        button.style.display="block";
+                        button.style.width="60%";
+                        button.style.backgroundColor="#555";
+                        button.style.textAlign="center";
+                        button.style.margin="10%";
+                        button.style.padding="10%";
+                        button.addEventListener('click', function () {
+                            that.do(event);
+                        })
+                        that.engine.getEngineVar("#DOM").appendChild(button);
+                    }
+                    that.engine.getEngineVar("#DOM").style.textAlign="center";
+                    that.engine.getEngineVar("#DOM").style.overflowY="auto";
+                }
+            },
+            {
+                name: "back", code: function (event) {
+                    CLOCKWORKRT.API.loadMenu();
+                }
+            },
+            {
+                name: "install", code: function (event) {
+                    CLOCKWORKRT.apps.installAppFromLocalFile(function () { 
+                        CLOCKWORKRT.API.loadMenu();
+                    }); 
+                }
+            },
+            {
+                name: "reset", code: function (event) {
+                    CLOCKWORKRT.apps.reset();
                 }
             }
         ]
